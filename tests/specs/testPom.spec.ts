e13f210e6c6fdf7d8ad2a57b5f67c543c869b3c3
valentin <mihalache.valentinflorian@gmail.com>
2025-09-15 10:12:28 +0300

    -Correct redundant code in spec

--- tests/specs/testPom.spec.ts
+++ tests/specs/testPom.spec.ts
@@ -4,10 +4,8 @@ import {getMainPagePom, CrawlerGenerate } from "../utils/spreeCrawler";
 test('should make sure the pom methods work', async ({ page }) => {
     const crawler = new CrawlerGenerate()
     const combined = await getMainPagePom(page)
-    await page.context().clearCookies()
     const mainPagePom = new MainPagePom(page)
     await mainPagePom.openUrl()
-    await page.goto("https://demo.spreecommerce.org/?t=" + Date.now(), { waitUntil: "domcontentloaded" });
     await page.waitForLoadState();
     await getMainPagePom(page)
     expect(crawler.validate(await combined.getProductLocator())).toBe(true);
