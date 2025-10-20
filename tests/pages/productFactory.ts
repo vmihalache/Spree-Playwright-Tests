f1c8c51e707d5f5f995d60b74084f167623f3eb9
valentin <mihalache.valentinflorian@gmail.com>
2025-10-20 16:08:51 +0300

    factory final changes

--- tests/pages/productFactory.ts
+++ tests/pages/productFactory.ts
@@ -3,11 +3,12 @@ import { GetRawData } from "./productCollector";
 import { ProductPagePom } from "./productPagePom";
 
 
-type productData = {
+export type productData = {
     'section.product.id': string,
     links: string,
     sizesAvailable: [],
     hasQuantity: boolean,
+    requiresSizeSelection: boolean,
     'section.product.link': string,
     'section.product.name': string,
     'section.title': string
@@ -19,6 +20,16 @@ type productData = {
     'section.product.price.hasDiscount': boolean,
     'section.product.price.discountPrice': string
     'section.product.price.regularPrice': string
+    'quantitySelectedMethod': string
+    'getCartPaneTotalValue': string
+    'getCheckoutTotalValue': string
+    'actions': {
+        getCartPaneTotalValue: () => Promise<number>
+        checkOutClick: () => Promise<void>
+        getCheckoutTotalValue: () => Promise<number>
+        pickASize: () => Promise<void>
+        getContext: () => Promise<any>
+    }
 }
 interface Product {
     getProductDetails(page: Page):
@@ -27,43 +38,59 @@ interface Product {
 class baseProductDetails implements Product {
     async getProductDetails(page: Page): Promise<productData[]> {
         const getRawData = new GetRawData(page)
-        await page.waitForLoadState() // optional safeguard
+        await page.waitForLoadState() 
         const productPom = new ProductPagePom(page)
         const basicListProducts = await getRawData.rawDataMethod()
-        // const extendedListProducts = await productPom.getProductsData(basicListProducts)
-        return await productPom.getProductsData(basicListProducts)
+        const extendedListProducts = await productPom.getProductsData(basicListProducts)
+        return extendedListProducts
+    }
+    protected filterProducts(products: productData[], requiresSize: boolean): productData[] {
+        return products.filter(product => {
+            const hasSize = product["sizesAvailable"].length > 0;
+            const hasQuantity = product["hasQuantity"] === true;
+
+            if (requiresSize) {
+                return hasQuantity && hasSize; 
+            } else {
+                return !hasSize && hasQuantity       
+            }
+        });
     }
 }
 
 class ProductWithQuantityForAtleastASize extends baseProductDetails {
     async getProductWithQuantityForAtleastASize(page: Page): Promise<productData[]> {
-        return baseProductDetails.getProductDetails(page)
+        return this.getProductDetails(page)
+    }
+    static async getResult(fullAugmentedList: productData[]): Promise<productData[]> {
+        const instance = new ProductWithQuantityForAtleastASize();
+        return instance.filterProducts(fullAugmentedList, true); 
     }
 }
 
 class ProductWithoutAnySizes extends baseProductDetails {
-    async getProductWithQuantityForAtleastASize(page: Page): Promise<productData[]> {
+    async getProductWithoutAnySizes(page: Page): Promise<productData[]> {
         return this.getProductDetails(page)
+    }
+    static async getResult(fullAugmentedList: productData[]): Promise<productData[]> {
+        const instance = new ProductWithoutAnySizes();
+        return instance.filterProducts(fullAugmentedList, false); 
     }
 }
 
-class ProductFactory {
+export class ProductFactory {
     readonly page: Page
     constructor(page: Page) {
         this.page = page
     }
     async createProduct() {
         const getProductData = new baseProductDetails()
-        const getResult = await getProductData.getProductDetails(this.page)
-        for (const element of getResult) {
-            if (element["hasQuantity"] === true) {
-                if (element["sizesAvailable"].length > 0) {
-                    return ProductWithQuantityForAtleastASize.
-                }
-            }
-            else {
-                return getResult
-            }
-        }
+        const allAugmentedProducts = await getProductData.getProductDetails(this.page)
+        const productsWithSize = await ProductWithQuantityForAtleastASize.getResult(allAugmentedProducts);
+        const productsWithoutSizes = await ProductWithoutAnySizes.getResult(allAugmentedProducts);
+
+        const validProducts = [...productsWithSize, ...productsWithoutSizes];
+        return validProducts;
     }
 }
+
