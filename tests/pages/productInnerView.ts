ae311b1b82bf32545be7bc7349b521dd62a4ed1f
valentin <mihalache.valentinflorian@gmail.com>
2025-10-20 15:32:02 +0300

    fix(integration): Restored correct, latest version of productInnerView.ts for stable integration

--- tests/pages/productInnerView.ts
+++ tests/pages/productInnerView.ts
@@ -1,65 +1,139 @@
 import { Locator, Page } from "playwright-core"
-
 export class ProductInnerView {
     readonly page: Page
     readonly sizeDropDown: Locator
-    readonly sizeAvailability: Locator
+    readonly quantitySelector: Locator
+    readonly inputLocator: Locator
+    quantitySelected: Number
     readonly quantityIncrease: Locator
     readonly quantityValue: Locator
     readonly addToCartButton: Locator
+    readonly cartPaneTotalValue: Locator
+    readonly checkoutButton: Locator
+    readonly checkoutTotalValue: Locator
+    readonly productImage: Locator
+    cachedSizes: Locator[]
+    getSizes: Locator[]
+    static lastTotalValue: any
     constructor(page: Page) {
         this.page = page
         this.sizeDropDown = this.page.locator('button[data-dropdown-target="button"]')
-        this.sizeAvailability = this.page.locator('[data-dropdown-target="menu"]').locator("label")
+        this.inputLocator = this.page.locator('[data-dropdown-target="menu"]')
+        this.quantitySelector = this.page.locator('[data-editor-name="Quantity Selector"]').getByText(/Only \d+ left/);
         this.quantityIncrease = this.page.locator('button[data-quantity-picker-target="increase"]')
         this.quantityValue = this.page.locator('input[data-quantity-picker-target="quantity"]')
         this.addToCartButton = this.page.locator('button[data-product-form-target="submit"]')
+        this.cartPaneTotalValue = this.page.locator('[data-controller="cart"]').getByText(" Total ").locator('span')
+        this.checkoutButton = this.page.locator('[data-cart-target="checkoutButton"]')
+        this.checkoutTotalValue = this.page.locator('[data-hook="order_summary"]').locator('span').nth(1)
+        this.productImage = this.page.locator(
+            '[data-pdp-desktop-gallery-target="imagesSlider"] img'
+        ).first();
+        this.getSizes = []
+        this.cachedSizes = []
+        this.quantitySelected = 0
     }
-
-    async getSizeAvailability() {
+    async drawerManagement() {
         try {
             const isDropdownVisible = await this.sizeDropDown.isVisible({ timeout: 3000 });
             if (!isDropdownVisible) {
-                console.log("Dropdown not visible");
                 return this.getEmptyResult();
             }
-            console.log("Dropdown is visible, proceeding to get sizes.");
-            await this.sizeDropDown.click(); // No force needed, as we waited for it.
-            const availableSizes = await this.sizeAvailability
-                .filter({ hasNot: this.page.locator(".cursor-not-allowed") })
-                .all();
-            if (availableSizes.length > 0) {
-                // console.log("Returning with available sizes.");
+            else {
+                await this.sizeDropDown.click(); 
+                for (const inpu of await this.inputLocator.all()) {
+                    this.cachedSizes = await inpu.locator("input:not(:checked) + label:not(.cursor-not-allowed)").all()
+                }
+            }
+        }
+        catch (error) {
+            return this.getEmptyResult();
+        }
+    }
+
+    async getSizeAvailability() {
+        const sizes = this.cachedSizes
+        if (Array.isArray(sizes)) {
+            if (sizes.length > 0) {
                 return {
-                    sizes: await Promise.all(availableSizes.map(el => el.innerText())),
+                    sizesAvailable: await Promise.all(sizes.map(el => el.innerText())),
                     hasQuantity: true
                 };
             }
             return this.getEmptyResult();
         }
-        catch (error) {
-
-            // console.log("No size dropdown found. This product may not have size options.");
-            return this.getEmptyResult();
-        }
     }
     private async getEmptyResult() {
         if (await this.quantityIncrease.isEnabled()) {
-            return { sizes: [], hasQuantity: true };
+            this.cachedSizes = []
+            return { sizesAvailable: [], hasQuantity: true };
         }
-        return { sizes: [], hasQuantity: false };
+        return { sizesAvailable: [], hasQuantity: false };
+    }
+
+    async increaseQuantity() {
+        await this.quantityIncrease.waitFor({ state: 'visible' });
+        let randomNumberOfProductsValue = Math.floor(Math.random() * 10) + 1
+        let randomBaseNumber = 1
+        if (await this.quantitySelector.isVisible()) {
+            const val = await this.quantitySelector.textContent()
+            if (val) {
+                randomBaseNumber = parseInt(val)
+            }
+        }
+        for (randomBaseNumber; randomBaseNumber < randomNumberOfProductsValue; randomBaseNumber++) {
+            if (await this.quantityIncrease.isEnabled()) {
+                await this.quantityIncrease.click()
+            }
+        }
+        this.quantitySelected = randomBaseNumber
+    }
+    async addToCart() {
+        const addToCartButton = await this.addToCartButton.all()
+        await addToCartButton[0].click()
+        await this.page.waitForTimeout(2000);
+    }
+    async getCartPaneTotalValue() {
+        const cartContainer = this.page.locator('.cart-summary-container');
+        await cartContainer.waitFor({ state: 'visible', timeout: 200000 });
+        const locator = this.cartPaneTotalValue;
+        await this.page.waitForTimeout(1000);
+        const updated = (await locator.textContent())?.trim();
+        return updated;
+    }
+    async checkOutClick() {
+        this.checkoutButton.click()
+    }
+    async getCheckoutTotalValue() {
+        const cartPaneLocator = this.checkoutTotalValue
+        await cartPaneLocator.waitFor({ state: 'visible' });
+        return cartPaneLocator.textContent()
+    }
+    async pickASize() {
+        const sizes = this.cachedSizes
+        if (Array.isArray(sizes)) {
+            if (sizes.length > 0) {
+                const randomLocator = sizes[Math.floor(Math.random() * sizes.length)];
+                await randomLocator.click();
+            }
+            else {
+                await this.getEmptyResult()
+            }
+        }
+
+    }
+    get quantitySelectedMethod() {
+        return this.quantitySelected
+    }
+    get actions() {
+        return {
+            pickASize: async () => this.pickASize(),
+            increaseQuantity: async () => this.increaseQuantity(),
+            addToCart: async () => this.addToCart(),
+            getCartPaneTotalValue: async () => this.getCartPaneTotalValue(),
+            checkOutClick: async () => this.checkOutClick(),
+            getCheckoutTotalValue: async () => this.getCheckoutTotalValue(),
+            quantitySelectedMethod: async () => this.quantitySelectedMethod
+        }
     }
 }
-// const size = allSizes[Math.floor(Math.random() * allSizes.length)];
-// await size.click()
-// if (await this.addToCartButton.getByText('Please choose Size').isVisible())
-// async increaseQuantity() {
-//     const randomNumberOfProducts = Math.floor(Math.random() * 10)
-//     for (let randomBaseNumber = 0; randomBaseNumber < randomNumberOfProducts; randomBaseNumber++) {
-//         await this.quantityIncrease.click()
-//     }
-// }
-// async addToCart() {
-//     await this.addToCartButton.click()
-//sizesAvailable: ["S","M"]
-//hasQuantity: true/false
